49db4f7d3966be6d1bea14697e8f44f9b0dfcd02
fernandomaltez <106553009+fernandomaltez@users.noreply.github.com>
2024-08-31 20:16:48 -0300

    Update 01-Vetores.c

--- Lista-Revisao/01-Vetores.c
+++ Lista-Revisao/01-Vetores.c
@@ -18,31 +18,25 @@ int main()
     }
     
     
-    //nesse caso o vetor possui uma quantidade par de espaços
+    //nesse caso o vetor possui um tamanho par
     if((n%2)==0)
     {
        for (i = 0; i < (n/2); i++) {
         temp = vetor[i];
-        vetor[i] = vetor[n - i];
-        vetor[n - i] = temp;
+        vetor[i] = vetor[n-i-1];
+        vetor[n-i-1] = temp;
        }
     }
     
-    //nesse caso o vetor possui um numero impar de casas
+    //nesse caso o vetor possui um tamanho ímpar
     else
     {
         for (i = 0; i < ((n+1)/2); i++) {
         temp = vetor[i];
-        vetor[i] = vetor[n - i];
-        vetor[n - i] = temp;
+        vetor[i] = vetor[n-i-1];
+        vetor[n-i-1] = temp;
         }
     }
-    
-        
-    
-    
-
-    
 
     printf("Vetor invertido:\n");
     for (i = 0; i < n; i++) {
