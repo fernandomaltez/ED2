14c0761ec50f4d04217e9a524fd174e46a9e0b5e
fernandomaltez <106553009+fernandomaltez@users.noreply.github.com>
2024-08-31 22:06:15 -0300

    Update 04-ListasDuplamenteLigadas.c

--- Lista-Revisao/04-ListasDuplamenteLigadas.c
+++ Lista-Revisao/04-ListasDuplamenteLigadas.c
@@ -1,26 +1,26 @@
-/*
-O código apresenta uma lista duplamente ligada com operações básicas para inserir, deletar e imprimir.
-
-1 - comente o código.
-2 - Modifique a operação de inserção de dados, para que o usuário seja capaz de inserir valores para a lista.
-3 - Faça uma operação de buscar por um valor específico, informado pelo usuário, onde o programa deve apresentar se o valor foi encontrado e, em caso afirmativo, exibir detalhes(vizinhos) do nó correspondente.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-//criando o tipo abstrato de dado "nó", que possui como atributos 
+//criando o tipo abstrato de dado "nó", que possui como atributos um valor, um ponteiro para o proximo nó e um ponteiro para o nó anterior
 typedef struct Node {
     int data;
     struct Node* next;
     struct Node* prev;
 } Node;
 
+//definindo o "nó raiz" da lista
 Node* head = NULL;
 
-void insertFront(int value) {
+//função que cria um novo "nó" e insere ele na lista
+void insertFront() 
+{
+    int num;
+    
+    printf("Digite um valor para esse nó :");
+    scanf("%d", &num);
+    
     Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = value;
+    newNode->data = num;
     newNode->next = head;
     newNode->prev = NULL;
     if (head != NULL)
@@ -28,6 +28,7 @@ void insertFront(int value) {
     head = newNode;
 }
 
+//função que remove o "nó"
 void deleteNode(Node* node) {
     if (node == head)
         head = node->next;
@@ -38,6 +39,7 @@ void deleteNode(Node* node) {
     free(node);
 }
 
+//função que imprime a lista
 void printList() {
     Node* temp = head;
     while (temp != NULL) {
@@ -47,9 +49,77 @@ void printList() {
     printf("\n");
 }
 
-int main() {
-    insertFront(10);
-    insertFront(20);
+void buscar()
+{
+    int elemento;
+    printf("Digite o valor a ser buscado: ");
+    scanf("%d", &elemento);
+
+    while (1)
+    {
+        int aux = 1;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            if (temp->data == elemento)
+            {
+                break;
+            }
+            temp = temp->next;
+            aux++;
+        }
+        
+        if(temp != NULL)
+        {
+            printf("Elemento %d encontrado na posição %d \n", elemento, aux);
+            printf("Elemento anterior: %d \n", temp->prev->data);
+            printf("Elemento seguinte: %d", temp->next->data);
+        }
+        
+        else
+        {
+            printf("Elemento %d não encontrado\n", elemento);
+        }
+       
+        
+        return;
+    }
+
+    
+}
+
+
+int main() 
+{
+    int n, aux2;
+    
+    printf("Digite a quantidade de valores que será inserido: ");
+    
+    scanf("%d", &n);
+    
+    for(int i = 0; i < n; i++)
+    {
+        insertFront();
+    }
+    
+    
     printList();
+    
+    printf("Deseja buscar um numero? Se sim digite 1, se não digite 2");
+    
+    scanf("%d", &aux2);
+    
+    if(aux2 == 1)
+    {
+        buscar();
+    }
+    
+    else
+    {
+        return 0;
+    }
+    
+    
+    
     return 0;
 }
